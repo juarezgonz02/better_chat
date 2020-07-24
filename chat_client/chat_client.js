d72141be7ae496852d6355a7c7407f5eaa10f8ef
Juarez <00126320@uca.edu.sv>
2020-07-23 22:59:30 -0600

    better variables

--- chat_client/chat_client.js
+++ chat_client/chat_client.js
@@ -1,20 +1,13 @@
 var permitir = true;
-var misCabeceras = new Headers();
-crearCheck();
-const popup = document.querySelector("input[name=checkbox]");
-var miInit = { method: 'GET',
+const misCabeceras = new Headers();
+const miInit = { method: 'GET',
                headers: misCabeceras,
                mode: 'no-cors',
                cache: 'default' };
 
-try{
-    popup.addEventListener('change', checking)
-    console.log(popup);
-}
-catch(e){
-    console.log(e);
-    console.log("No se ha detectado mensaje");
-}
+crearCheck();   
+const popup = document.querySelector("input[name=checkbox]");
+popup.addEventListener('change', checking);
 
 function checking(event){
     const status = event.target.checked;
@@ -27,14 +20,19 @@ function checking(event){
 }
 
 function leerChat (){
-    console.log("Leyendo...")
-    var chat = document.getElementsByClassName("oIy2qc");
-    var tamano = chat.length-1;
-    var message = chat[tamano].dataset.messageText;
-    var ultimoMessage = message.toLowerCase();
+    const chat = document.getElementsByClassName("oIy2qc");
+    const tamano = chat.length-1;
+    const message = chat[tamano].dataset.messageText;
+    const ultimoMessage = message.toLowerCase();
     if(permitir){
-        if(ultimoMessage == "pausar"||ultimoMessage == "pausa"||ultimoMessage == "avanza"|| ultimoMessage == "avanzar" ){
-            console.log("Ultimo mensaje:  "+ message);
+        console.log("Leyendo...")
+        if(
+        ultimoMessage == "#pausar" ||
+        ultimoMessage == "#pausa"  ||
+        ultimoMessage == "#avanza" ||
+        ultimoMessage == "#avanzar" )
+        {
+            console.log("Ultimo mensaje:  "+ message);              
             control();
             chat[tamano].dataset.messageText = "";
         }
@@ -44,23 +42,36 @@ function leerChat (){
     }
 
 
-}function detener(){
+}
+
+function detener(){
     permitir = false;
+    const chat = document.getElementsByClassName("oIy2qc");
+    const tamano = chat.length-1;
+    chat[tamano].dataset.messageText = "";
     console.log("SE HA DETENIDO LA LECTURA DEL CHAT");
 
 }
+
 function continuar(){
     permitir = true;
     setInterval(leerChat,2000);
 }
+
 function control(){
-    fetch('http://localhost:5000/control',miInit)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      console.log(myJson[0]);
-    })
+    try {
+        fetch('http://localhost:5000/control',miInit)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(myJson) {
+        console.log(myJson);
+        })
+    } catch (error) {
+        console.log("Por favor enciende el servidor de control")
+    }
+    
 }
+
 alert("------SE HA INICIADO EL CHAT BOT-------- ");
 
